c0ce38e7c381737f00f168a886e07e41f256e3e0
武新飞 <xinfei.wu@qq.com>
2020-03-03 18:56:10 +0800

    load hosts

---
+++ hosts.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/goodhosts/hostsfile"
+)
+
+func init() {
+	customPath := "./hosts"
+	if _, err := os.Stat(customPath); err != nil {
+		return
+	}
+
+	customHosts, err := hostsfile.NewCustomHosts(customPath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	sysHosts, err := hostsfile.NewHosts()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, line := range customHosts.Lines {
+		if err = sysHosts.Add(line.IP, line.Hosts...); err != nil {
+			log.Printf("failed to add line: %s", line.Raw)
+		}
+	}
+}
